60a4ddd85908dd76c67161680fc9d701f57448e3
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-05-17 14:58:04 +0900

    format.

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -41,40 +41,40 @@ export const loginState: ILoginState = {
 
 export const LoginView = ({ state, actions }: { state: ILoginState, actions: ILoginActions }) => (
   <div class={ classNames("ui middle aligned center aligned grid", { loading: state.isLoading }) }>
-  <div class="column" style={{ maxWidth: "450px" }}>
-    <h2 class="ui teal image header">
-      <div class="content">ログイン</div>
-    </h2>
-    <form class={ classNames("ui large form", { loading: state.isLoading }) }>
-      <div class="ui stacked segment">
-        <div class="field">
-          <div class="ui left icon input">
-            <i class="user icon"></i>
-            <input type="text" placeholder="メールアドレス"
-              value={ state.formData.username }
-              oninput={ (e: InputEvent ) =>
-                ({ formData: Object.assign(state.formData, { username: e.target.value }) }) }
-              oncreate={ (e: HTMLInputElement) => e.focus() } />
+    <div class="column" style={{ maxWidth: "450px" }}>
+      <h2 class="ui teal image header">
+        <div class="content">ログイン</div>
+      </h2>
+      <form class={ classNames("ui large form", { loading: state.isLoading }) }>
+        <div class="ui stacked segment">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input type="text" placeholder="メールアドレス"
+                value={ state.formData.username }
+                oninput={ (e: InputEvent ) =>
+                  ({ formData: Object.assign(state.formData, { username: e.target.value }) }) }
+                oncreate={ (e: HTMLInputElement) => e.focus() } />
+            </div>
           </div>
-        </div>
-        <div class="field">
-          <div class="ui left icon input">
-            <i class="lock icon"></i>
-            <input type="password" placeholder="パスワード"
-              value={ state.formData.password }
-              oninput={ (e: InputEvent ) =>
-                ({ formData: Object.assign(state.formData, { password: e.target.value }) }) }
-              oncreate={ (e: HTMLInputElement) => e.focus() } />
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="lock icon"></i>
+              <input type="password" placeholder="パスワード"
+                value={ state.formData.password }
+                oninput={ (e: InputEvent ) =>
+                  ({ formData: Object.assign(state.formData, { password: e.target.value }) }) }
+                oncreate={ (e: HTMLInputElement) => e.focus() } />
+            </div>
           </div>
+          <div class="ui fluid large teal submit button"
+            onclick={ actions.submit }>ログイン</div>
         </div>
-        <div class="ui fluid large teal submit button"
-          onclick={ actions.submit }>ログイン</div>
+        <div class="ui error message"></div>
+      </form>
+      <div class="ui message">
+        または <Link to="/register">新規登録</Link>
       </div>
-      <div class="ui error message"></div>
-    </form>
-    <div class="ui message">
-      または <Link to="/register">新規登録</Link>
     </div>
   </div>
-</div>
 );
